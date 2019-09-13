4c0d6b3dc1475fff0fb052ac1470a3ef71c032a7
YoelK <yoelkastro@gmail.com>
2019-09-13 15:52:01 +0100

    Replicated previous library folders

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+      long_description = fh.read()
+
+
+print("                                                                                 ")
+print("*********************************************************************************")
+print("         PiWarsTurkiyeRobotKiti2019 kutuphanesine hosgeldiniz!")
+print("         Gerekli dokumantosyonu https://github.com/HisarCS/PiWarsTurkey-Library-Folders adresinden bulabilirsiniz.")
+print("         Herhangi bir sorun cikarsa once yukaridaki dokumantosyona goz atmanizi oneririz. Eger sorun hala devam ederse bize ulasmaktan cekinmeyin.")
+print("         Bu kutuphaneyi indirirken de kullanirken de programin sudo ile calistigindan emin olun.")
+print("*********************************************************************************")
+print("                                                                                 ")
+
+setup(
+    name = "PiWarsTurkiyeRobotKiti2019",
+    version = "1.1.4",
+    author = "Yasar İdikut, Sarp Yoel Kastro",
+    author_email = "yasar.idikut@hisarschool.k12.tr, sarp.kastro@hisarschool.k12.tr",
+    description = "Library that makes use of sensors, motors, and servos in the PiWars Turkey robot kit by HisarCS",
+    packages = ["PiWarsTurkiyeRobotKiti2019"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=["Development Status :: 4 - Beta"],
+    install_requires=[
+#        'picamera',
+#        'pygame',
+#        'RPi.GPIO',
+#        'wiringpi',
+	'Click'
+    ],
+    entry_points='''
+        [console_scripts]
+        piwarstr=CommandLineTools:main
+    '''
+
+)
